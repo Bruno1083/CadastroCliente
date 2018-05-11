f50aefc7b371668e46ac48eca931aad4d23027b4
Bruno Cavalcante <cavalcante1083@gmail.com>
2018-05-11 16:42:31 -0300

    alteracoes

--- CadastrodeClientes/app/src/main/java/com/example/manuelmartins/cadastrodeclientes/cadastro.java
+++ CadastrodeClientes/app/src/main/java/com/example/manuelmartins/cadastrodeclientes/cadastro.java
@@ -2,12 +2,14 @@ package com.example.manuelmartins.cadastrodeclientes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class cadastro extends AppCompatActivity {
     EditText codigo,nome,endereco,cpf,telefone,email;
     Button inserir,consultar,remover,alterar;
+    private Repositorio rep;
 
 
     @Override
@@ -29,7 +31,28 @@ public class cadastro extends AppCompatActivity {
         remover = (Button) findViewById(R.id.btRemover);
         alterar = (Button) findViewById(R.id.btAlterar);
 
+        rep = new Repositorio(100);
+
+        inserir.setOnClickListener((View.OnClickListener) this);
+        consultar.setOnClickListener((View.OnClickListener) this);
+        remover.setOnClickListener((View.OnClickListener) this);
+        alterar.setOnClickListener((View.OnClickListener) this);
+
 
     }
+
+    public void limpa_campos(){
+
+        codigo.setText("");
+        nome.setText("");
+        endereco.setText("");
+        cpf.setText("");
+        telefone.setText("");
+        email.setText("");
+
+    }
+
+    
+
 
 }
